1a2e20a4349265dd330177270a6e5286c484d1fb
Luiz Guilherme D'Abruzzo Pereira <luiz290788@gmail.com>
2021-05-03 12:45:41 +0000

    add gatsby

--- apps/luiz-guilherme.com/gatsby-config.js
+++ apps/luiz-guilherme.com/gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
+    // `gatsby-transformer-sharp`,
     {
       resolve: require.resolve(`@nrwl/gatsby/plugins/nx-gatsby-ext-plugin`),
       options: {
